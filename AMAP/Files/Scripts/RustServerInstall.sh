81ee07391784984b398be58de7e38492043102c3
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-11 16:45:36 -0600

    Rust installer

--- AMAP/Files/Scripts/RustServerInstall.sh
+++ AMAP/Files/Scripts/RustServerInstall.sh
@@ -2,8 +2,8 @@
 DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source "$DIR/config.sh"
 
-
 echo "This is going to the install of the rust server"
-wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh rustserver ;;
-$USER ./rustserver install;;
 
+# Download and install the rust server then run 
+wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh rustserver
+$TARGET_DIR/./rustserver install
