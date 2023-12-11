fe003c201fa1f19f6250920cae891ddb76e6065b
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-11 17:47:35 -0600

    Update RustServerInstall.sh

--- AMAP/Files/Scripts/RustServerInstall.sh
+++ AMAP/Files/Scripts/RustServerInstall.sh
@@ -6,11 +6,10 @@ echo "This is going to the install of the rust server"
 
 # Download and install the rust server then run 
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh rustserver
-sleep 3
 echo "Install Completed" 
 echo "Moving files" 
 $RUSTMOVE
 echo "Files have been moved"
 echo "Starting Rust Server Install" 
-/home/$USERNAME/./rustserver install
+$SERVER install
 
