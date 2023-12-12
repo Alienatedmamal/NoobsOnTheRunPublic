9ebbc7c595b2d5f4910afc81c2b2ec469059ec4e
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-12 12:04:04 -0600

    Update AMAPNC.sh

--- AMAP/AMAPNC.sh
+++ AMAP/AMAPNC.sh
@@ -3,7 +3,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/config.sh
 
 # Options
-
+clear
 cat $DIR/$AMAPLOGO
 cat $DIR/$OPTIONS
 
@@ -296,8 +296,9 @@ while true; do
                                         echo "2. Update Rust Plugins"
                                         echo "3. Install Rust Server"
                                         echo "4. Install Oxide"
-										echo "5. Install Plugins"
-                                        echo "5. Return to Menu"
+					echo "5. Install Plugins"
+     					echo "6. Create Server Config File"
+                                        echo "7. Return to Menu"
 
                         read -p "Enter your choice: " subchoice
                                 case $subchoice in
@@ -338,8 +339,22 @@ while true; do
                               echo "Server MODS Install aborted." && $AMAP
                               fi
                               ;;
-                        5) echo "Feature coming soon" && $AMAP $$ exit ;;
-						6) echo "Going Back " && clear && $AMAP && exit ;;
+                        5) read -p "Are you sure you want to install Plugins? (Y/n): " confirm_PluginMove
+                		if [ "$confirm_PluginMove" == "Y" ]; then
+                        	$PLUGINMOVE && $AMAPNC && exit
+                		else
+                        	echo "Plugin Install Aborted." && $AMAPNC
+                		fi
+		  		;;
+      			6) read -p "Are you sure you want to create a Server config file? (Y/n): " confirm_Serverconfig
+                		if [ "$confirm_Serverconfig" == "Y" ]; then
+                        	$SERVERCONFIG && $AMAPNC && exit
+                		else
+                        	echo "Server Configurator Aborted." && $AMAPNC
+                		fi
+		  		;;
+	  
+                        7) echo "Going Back " && clear && $AMAP && exit ;;
                         *)echo "Invalid choice. Please enter a valid sub-option." ;;
                                                 esac
                                                 read -p "Press Enter to continue..."
