409501b5bbbc8bfffc97c35ed0ed57b425ce44bf
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-13 11:11:40 -0600

    Update LogCleaner.sh

--- AMAP/Files/Scripts/LogCleaner.sh
+++ AMAP/Files/Scripts/LogCleaner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source $DIR/config.sh
-SCRIPT="LogCleaner:"
+
 
 # Yuup just need it to do one thing...Clean the logs. 
 echo > $LOGS
-echo "$(date)" $SCRIPT Logs have been cleared >> $LOGS || echo "$(date)" $SCRIPT Failed to clear logs.. >> $LOGS
+echo "$(date)" $SCRIPTLOGCLEAN Logs have been cleared >> $LOGS || echo "$(date)" $SCRIPTLOGCLEAN Failed to clear logs.. >> $LOGS
