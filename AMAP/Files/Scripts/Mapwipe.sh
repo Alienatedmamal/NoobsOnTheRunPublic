655cea911995620b0b19ca7d66c608bbe6415724
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-13 11:12:44 -0600

    Update Mapwipe.sh

--- AMAP/Files/Scripts/Mapwipe.sh
+++ AMAP/Files/Scripts/Mapwipe.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source "$DIR/config.sh"
-SCRIPT="MapeWipe:"
+
 
 #Stops Server, Performs Map Wipe.  
-	$SAYDATE $SCRIPT $SERVERNAME Server will now stop >> $LOGS
+	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop >> $LOGS
 $USER $SERVER stop >> $LOGS
-	$SAYDATE $SCRIPT $SERVERNAME Server Has Stopped >> $LOGS || 
-	$SAYDATE $SCRIPT $SERVERNAME Server Has Failed to Stop >> $LOGS  
-	$SAYDATE $SCRIPT $SERVERNAME Server Map will now wipe  >> $LOGS 
+	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Stopped >> $LOGS || 
+	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Failed to Stop >> $LOGS  
+	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Map will now wipe  >> $LOGS 
 $USER $SERVER map-wipe >> $LOGS 
-	$SAYDATE $SCRIPT $SERVERNAME Map wipe has been completed.  >> $LOGS || 
-	$SAYDATE $SCRIPT $SERVERNAMEMap has FAILED to Wipe  >> $LOGS 
+	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Map wipe has been completed.  >> $LOGS || 
+	$SAYDATE $SCRIPTMAPWIPE $SERVERNAMEMap has FAILED to Wipe  >> $LOGS 
 	sleep 1 
-	$SAYDATE $SCRIPT Automation Map wipe completed  >> $LOGS 
-	$SAYDATE $SCRIPT Starting Server Now.....  >> $LOGS 
+	$SAYDATE $SCRIPTMAPWIPE Automation Map wipe completed  >> $LOGS 
+	$SAYDATE $SCRIPTMAPWIPE Starting Server Now.....  >> $LOGS 
 
