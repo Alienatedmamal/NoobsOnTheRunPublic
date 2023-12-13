02943c9a5059b2ddf716047a8469a68acbf88a70
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-13 11:25:55 -0600

    Update ServerStart.sh

--- AMAP/Files/Scripts/ServerStart.sh
+++ AMAP/Files/Scripts/ServerStart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source "$DIR/config.sh"
-SCRIPT="ServerStart:"
+
 
 
 #This is to start the rust server. 
-	$USER $SERVER start >> $LOGS ;
-        $SAY $SCRIPT $SERVERNAME Server Has Started >> $LOGS ||
-        $SAY $SCRIPT $SERVERNAME Server Has Failed To Start >> $LOGS
+	$USER $SERVER start >> $LOGS 
+        $SAY $SCRIPTSERVERSTART $SERVERNAME Server Has Started >> $LOGS ||
+        $SAY $SCRIPT$SCRIPTSERVERSTART $SERVERNAME Server Has Failed To Start >> $LOGS
