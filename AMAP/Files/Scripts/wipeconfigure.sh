d26c605a5cf4b71d7452126e3aaf4881ab40528c
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-12 21:07:34 -0600

    Update wipeconfigure.sh

--- AMAP/Files/Scripts/wipeconfigure.sh
+++ AMAP/Files/Scripts/wipeconfigure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+DIR="$(cd "$(dirname "$0")/../../" && pwd)"
+source "$DIR/config.sh"
 # Ask for user input
 read -p "Enter the Year : " YR
 read -p "Enter the Month : " MN
@@ -12,19 +13,19 @@ read -p "Enter the old Day: " ODY
 read -p "Is it a Full-wipe or a Map-wipe? (Type 'Full' or 'Map'): " WIPE_TYPE
 
 # Define the output file path
-OUTPUT_FILE="/home/alienatedmammal/Documents/RustBackups/wipescripts/WipeOutput.txt"
+
 
 # Generate output based on the wipe type
 if [ "$WIPE_TYPE" == "Full" ]; then
    WIPE_COMMAND="commands[\"20$OYR-$OMN-$ODY\"]=\"/home/alienatedmammal/Documents/RustBackups/wipescripts/./Fullwipe.sh && sed -i 's/$OS/$NS/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OS/$NS/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/serverfiles/oxide/config/ServerInfo.json ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/serverfiles/oxide/config/SmartChatBot.json ; echo Files have been copied >> /home/alienatedmammal/Documents/RustBackups/wipescripts/Logs/Logs.txt || echo Failed to copy files >> /home/alienatedmammal/Documents/RustBackups/wipescripts/Logs/Logs.txt && /home/alienatedmammal/Documents/RustBackups/wipescripts/./ServerStart.sh" >> "$OUTPUT_FILE"
-echo "$WIPE_COMMAND" >> "$OUTPUT_FILE"
+echo "$WIPE_COMMAND" >> "$WIPER"
 elif [ "$WIPE_TYPE" == "Map" ]; then
     WIPE_COMMAND="commands[\"20$OYR-$OMN-$ODY\"]=\"/home/alienatedmammal/Documents/RustBackups/wipescripts/./Mapwipe.sh && sed -i 's/$OS/$NS/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OS/$NS/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/common.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/lgsm/config-lgsm/rustserver/rustserver.cfg ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/serverfiles/oxide/config/ServerInfo.json ; sed -i 's/$OMN\/$ODY\/$OYR/$MN\/$DY\/$YR/g' /home/alienatedmammal/serverfiles/oxide/config/SmartChatBot.json ; echo Files have been copied >> /home/alienatedmammal/Documents/RustBackups/wipescripts/Logs/Logs.txt || echo Failed to copy files >> /home/alienatedmammal/Documents/RustBackups/wipescripts/Logs/Logs.txt && /home/alienatedmammal/Documents/RustBackups/wipescripts/./ServerStart.sh" >> "$OUTPUT_FILE"
- echo "$WIPE_COMMAND" >> "$OUTPUT_FILE"
+ echo "$WIPE_COMMAND" >> "$WIPER"
 else
     echo "Invalid wipe type. Please enter 'Full' or 'Map'."
     exit 1
 fi
 
 # Provide feedback to the user
-echo "Wipe details have been written to $OUTPUT_FILE"
+echo "Wipe details have been written to $SWIPER"
