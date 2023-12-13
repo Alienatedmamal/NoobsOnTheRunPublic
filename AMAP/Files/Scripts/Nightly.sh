acec73162d31f535a839da74f895ba4441db3569
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-13 08:16:52 -0600

    Update Nightly.sh

--- AMAP/Files/Scripts/Nightly.sh
+++ AMAP/Files/Scripts/Nightly.sh
@@ -8,14 +8,15 @@ SCRIPTNAME="Nightly:"
 	$SAYDATE $SCRIPTNAME Server will now stop >> $LOGS &&
 $USER $SERVER stop >> $LOGS &&
 	$SAYDATE $SERVERNAME $SCRITPNAME Server Has Stopped >> $LOGS || 
-	$SAYDATE $SERVERNAME $SCRITPNAME Server Has Failed to Stop >> $LOGS ; 
-	$SAYDATE $SERVERNAME $SCRITPNAME will Now Update >> $LOGS &&
-$USER $SERVER update >> $LOGS &&
-	$SAYDATE $SERVERNAME $SCRITPNAME Server Has Updated >> $LOGS || 
-	$SAYDATE $SERVERNAME $SCRITPNAME Server Failed To Update >> $LOGS ; 
-	$SAYDATE $SERVERNAME $SCRITPNAME Server MODS Will Update >> $LOGS ;
-$USER $SERVER mods-update >> $LOGS &&
-	$SAYDATE $SERVERNAME $SCRITPNAME Server Mods Have Been Updated >> $LOGS || 
-	$SAYDATE $SERVERNAME $SCRITPNAME Server Mods Have Failed to Update && 
-    $SAYDATE $SCRITPNAME Nightly Restart Completed. Restarting Now.... >> $LOGS 
+	$SAYDATE $SERVERNAME $SCRITPNAME Server Has Failed to Stop >> $LOGS 
+# Currently this stops any plugins from working after an update. Working on a fix. See GitHub for more information 
+#	$SAYDATE $SERVERNAME $SCRITPNAME will Now Update >> $LOGS &&
+#$USER $SERVER update >> $LOGS &&
+#	$SAYDATE $SERVERNAME $SCRITPNAME Server Has Updated >> $LOGS || 
+#	$SAYDATE $SERVERNAME $SCRITPNAME Server Failed To Update >> $LOGS ; 
+#	$SAYDATE $SERVERNAME $SCRITPNAME Server MODS Will Update >> $LOGS ;
+#$USER $SERVER mods-update >> $LOGS &&
+#	$SAYDATE $SERVERNAME $SCRITPNAME Server Mods Have Been Updated >> $LOGS || 
+#	$SAYDATE $SERVERNAME $SCRITPNAME Server Mods Have Failed to Update && 
+#    $SAYDATE $SCRITPNAME Nightly Restart Completed. Restarting Now.... >> $LOGS 
 
