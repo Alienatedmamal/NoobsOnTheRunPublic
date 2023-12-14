f7b597ea007541ffcd9b4f395d63ea6fea338729
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-14 10:40:36 -0600

    V2 Incoming

--- AMAP/Files/Scripts/LogCleaner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "$0")/../../" && pwd)"
-source $DIR/config.sh
-
-
-# Yuup just need it to do one thing...Clean the logs. 
-echo > $LOGS
-echo "$(date)" $SCRIPTLOGCLEAN Logs have been cleared >> $LOGS || echo "$(date)" $SCRIPTLOGCLEAN Failed to clear logs.. >> $LOGS
--- config.sh
+++
@@ -1,8 +0,0 @@
-# config.sh
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-
-FILELOC="AMAP/Files/Scripts"
-FILEMAIN="AMAP"
-CHECKUSER="Scripts/checkuser"
-USER="Scripts/./checkuser"
