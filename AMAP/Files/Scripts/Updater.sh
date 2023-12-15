2825bf1e4cc1d07f0d71fe6704cd62337af59477
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 17:04:41 -0600

    Update Updater.sh

--- AMAP/Files/Scripts/Updater.sh
+++ AMAP/Files/Scripts/Updater.sh
@@ -5,12 +5,12 @@ source "$DIR/config.sh"
 echo "Getting Updates Ready...."
 sleep 1
 echo "Downloading Updates...."
-git clone https://github.com/Alienatedmamal/Updater.git "$DIR/Files/Scripts/Updater"
+git clone https://github.com/Alienatedmamal/Updater.git
 echo "Download Completed"
 sleep 1
 echo "Moving Updates...."
 sleep 1
-#mv -f "$DIR/Files/Scripts/Updater/Update" "$DIR/Files/"
+mv -f "$DIR/Files/Scripts/Updater" "$DIR/Files/"
 sleep 1
 echo "Files have moved" || echo "Files have FAILED to move"
 chmod +x "$DIR/Files/Update/update.sh"
