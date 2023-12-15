a844a97de1f04e112251cea962b8fff28a8f1de0
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 12:14:38 -0600

    v2.0.5

--- AMAP/Files/Scripts/Fullwipe.sh
+++ AMAP/Files/Scripts/Fullwipe.sh
@@ -4,22 +4,12 @@ source "$DIR/config.sh"
 
 
 #Stops Server, Updates, Updates Mods, Performs Full Wipe.  
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now stop >> $LOGS &&
+	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now Full wipe >> $LOGS
+$USER $SERVER full-wipe >> $LOGS 
+  	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Been Full-wiped>> $LOGS || 
+	$SAYDATE $SCRIPTFULL $SERVERNAME Full-wipe Has Failed to Wipe   
+
+ 	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now stop >> $LOGS &&
 $USER $SERVER stop >> $LOGS &&
 	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Stopped >> $LOGS || 
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Failed to Stop >> $LOGS ; 
-
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now update >> $LOGS &&
-$USER $SERVER update >> $LOGS &&
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Updated >> $LOGS || 
-	$SAYDATE $SERVERNAME Server Failed To Update >> $LOGS ; 
-
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server mods will update >> $LOGS ;
-$USER $SERVER mods-update >> $LOGS &&
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server Mods Have Been Updated >> $LOGS || 
-	$SAYDATE $SCRIPTFULL $SERVERNAME Server Mods Have Failed to Update
-
- 	$SAYDATE $SCRIPTFULL $SERVERNAME Server will now Full wipe >> $LOGS
-  $USER $SERVER full-wipe >> $LOGS 
-  	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Been Full-wiped>> $LOGS || 
-	$SAYDATE $SCRIPTFULL $SERVERNAME Full-wipe Has Failed to Wipe    
+	$SAYDATE $SCRIPTFULL $SERVERNAME Server Has Failed to Stop >> $LOGS   
