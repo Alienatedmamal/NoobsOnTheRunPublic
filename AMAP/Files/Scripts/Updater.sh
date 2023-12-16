cbd9d8860067f28bc6d9488c98385fe1533ccf76
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-15 21:49:21 -0600

    Update Updater.sh

--- AMAP/Files/Scripts/Updater.sh
+++ AMAP/Files/Scripts/Updater.sh
@@ -2,7 +2,23 @@
 DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source "$DIR/config.sh"
 
+# Version Number
+desired_version="2.0.6"
 
+# Specify the path to the file containing the version number
+version_file="$DIR/Files/version.txt"
+
+# Check if the file exists
+if [ -e "$version_file" ]; then
+    # Read the version number from the file
+    current_version=$(cat "$version_file")
+
+    # Compare the current version with the desired version
+    if [ "$current_version" = "$desired_version" ]; then
+    echo "Version is already up to date. Stopping further actions."
+    sleep 3
+        exit 0  # Exit with success status
+    else
 echo "Getting Updates Ready"
 sleep 2
 echo "Clear old updates...."
@@ -29,6 +45,35 @@ echo "Updater is now ready to start...." || echo "Updater Failed to execute...."
 sleep 2
 echo "Starting Updater...."
 $DIR/Updater/Update/./update.sh
-
 echo "Updates completed" || echo "Updates Failed"
-exit 0
+fi
+else
+echo "Getting Updates Ready"
+sleep 2
+echo "Clear old updates...."
+updater_directory="$DIR/Updater/"
+
+# Check if the directory exists
+if [ -d "$updater_directory" ]; then
+    # Directory exists, remove it
+    rm -fr "$updater_directory"
+    echo "Updater directory deleted."
+else
+    echo "Updater directory does not exist."
+fi
+sleep 2
+echo "Downloading latest Updates....."
+sleep 2
+git clone https://github.com/Alienatedmamal/Updater.git
+echo "Download Completed" || echo "Failed to Download"
+sleep 2
+echo "File move complted.." || echo "Failed to move files"
+sleep 2
+chmod +x $DIR/Updater/Update/update.sh
+echo "Updater is now ready to start...." || echo "Updater Failed to execute...."
+sleep 2
+echo "Starting Updater...."
+$DIR/Updater/Update/./update.sh
+echo "Updates completed" || echo "Updates Failed"
+fi
+
