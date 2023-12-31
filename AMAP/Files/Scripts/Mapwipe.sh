475862150c97b6137353f90085e8d62155f8283f
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2023-12-31 12:21:10 -0600

    Update Mapwipe.sh
    Fixed the map-wipe

--- AMAP/Files/Scripts/Mapwipe.sh
+++ AMAP/Files/Scripts/Mapwipe.sh
@@ -3,16 +3,15 @@ DIR="$(cd "$(dirname "$0")/../../" && pwd)"
 source "$DIR/config.sh"
 
 
-#Stops Server, Performs Map Wipe.  
-	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop >> $LOGS
+#Stops Server, Performs Map Wipe.
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server will now stop >> $LOGS
 $USER $SERVER stop >> $LOGS
-	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Stopped >> $LOGS || 
-	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Failed to Stop >> $LOGS  
-	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Map will now wipe  >> $LOGS 
-$USER $SERVER map-wipe >> $LOGS 
-	$SAYDATE $SCRIPTMAPWIPE $SERVERNAME Map wipe has been completed.  >> $LOGS || 
-	$SAYDATE $SCRIPTMAPWIPE $SERVERNAMEMap has FAILED to Wipe  >> $LOGS 
-	sleep 1 
-	$SAYDATE $SCRIPTMAPWIPE Automation Map wipe completed  >> $LOGS 
-	$SAYDATE $SCRIPTMAPWIPE Starting Server Now.....  >> $LOGS 
-
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Stopped >> $LOGS ||
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Has Failed to Stop >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Server Map will now wipe  >> $LOGS
+rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/proceduralmap* >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAME Map wipe has been completed.  >> $LOGS ||
+        $SAYDATE $SCRIPTMAPWIPE $SERVERNAMEMap has FAILED to Wipe  >> $LOGS
+        sleep 1
+        $SAYDATE $SCRIPTMAPWIPE Automation Map wipe completed  >> $LOGS
+        $SAYDATE $SCRIPTMAPWIPE Starting Server Now.....  >> $LOGS
