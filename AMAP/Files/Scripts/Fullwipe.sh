419a451fff5932bcd403d4d6e1883ef031bc26fd
AlienatedMammal <86496403+Alienatedmamal@users.noreply.github.com>
2024-01-06 10:21:14 -0600

    Update Fullwipe.sh

--- AMAP/Files/Scripts/Fullwipe.sh
+++ AMAP/Files/Scripts/Fullwipe.sh
@@ -9,9 +9,13 @@ $USER $SERVER stop >> $LOGS
         $SAYDATE $SCRIPTFULL $SERVERNAME Server Has Stopped >> $LOGS ||
         $SAYDATE $SCRIPTFULL $SERVERNAME Server Has Failed to Stop >> $LOGS
         $SAYDATE $SCRIPTFULL $SERVERNAME Server will now update.... >> $LOGS
- $USER $SERVER update >> $LOGS
- $SAYDATE $SCRIPTFULL $SERVERNAME Server has updated >> $LOGS ||
- $SAYDATE $SCRIPTFULL $SERVERNAME Server has failed to update >> $LOGS
+$USER $SERVER update >> $LOGS
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server has updated >> $LOGS ||
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server has failed to update >> $LOGS
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server Plugins will now update... >> $LOGS
+$USER $SERVER mods-update >> $LOGS
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server Plugins have updated >> $LOGS ||
+        $SAYDATE $SCRIPTFULL $SERVERNAME Server Plugins failed to update >> $LOGS
         $SAYDATE $SCRIPTFULL $SERVERNAME Server will now begin full wipe.... >> $LOGS
         $SAYDATE $SCRIPTFULL $SERVERNAME Removing Players Data..... >> $LOGS
 rm -fr /home/$USERNAME/serverfiles/server/$HOSTNAME/player* >> $LOGS
